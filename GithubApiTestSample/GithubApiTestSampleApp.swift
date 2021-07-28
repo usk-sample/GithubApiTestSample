29eb8428bee328ea577e5bb4069d5585b957cfee
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-29 08:47:04 +0900

    Initial Commit

---
+++ GithubApiTestSample/GithubApiTestSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApiTestSampleApp.swift
+//  GithubApiTestSample
+//
+//  Created by Yusuke Hasegawa on 2021/07/29.
+//
+
+import SwiftUI
+
+@main
+struct GithubApiTestSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
