d465d4f1b95fe41df1fef89f5cc60684c8f53b45
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-07-29 08:57:26 +0900

    feat: preparing Unit Tests

--- GithubApiTestSampleTests/GithubApiTestSampleTests.swift
+++ GithubApiTestSampleTests/GithubApiTestSampleTests.swift
@@ -6,7 +6,11 @@
 //
 
 import XCTest
+
+import ViewInspector
+
 @testable import GithubApiTestSample
+
 
 class GithubApiTestSampleTests: XCTestCase {
 
@@ -17,17 +21,18 @@ class GithubApiTestSampleTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testViewModel() throws {
+        
+        
+        
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
